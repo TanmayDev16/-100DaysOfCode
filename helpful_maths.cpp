4dcbc3d18e0bcc2bc69c1610aa2f5ce8197eb9c8
Tanmay <srivastava.tanmay.162003@gmail.com>
2022-09-21 08:31:24 +0530

    new

--- helpful_maths.cpp
+++ helpful_maths.cpp
@@ -1,5 +1,6 @@
-#include <iostream>//use  of stoi function 
+#include <iostream>//use  of standard library function of c++ string
 #include<string>
+#include<algorithm>//***very important step to use sort function***
 using namespace std;
 
 int main() {
@@ -8,14 +9,11 @@ int main() {
   
     
     cin>>s;
-    // int n= (s.length()+1)/2;
-    // int arr[n];
-    
+  //removing plus sign
     s.erase(remove(s.begin(),s.end(),'+'),s.end());
-
-    // cout<<s; 
     int n=s.length();
     int arr[n];
+    // storing it in an array in integer format
     for(int i=0;i<s.length();i++){
         int temp=((int)s[i]);
         arr[i]=temp;
@@ -24,8 +22,9 @@ int main() {
     
     
     }
+    //sorting the number array
     sort(arr,arr+n);
- 
+ //creating a string and converting the number to string by adding + sign 
     string finale;
 
     for(int i=0;i<s.length();i++){
@@ -35,7 +34,8 @@ int main() {
             
   
     }
-
-    cout<<char(65);
+//removing last element from finale string
+string st = finale.substr(0, finale.size()-1);
+    cout<<st;
     return 0;
 }
